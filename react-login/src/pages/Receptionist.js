5d8e3ad3339f1bb3f66060cea52df5c1a4bdbc28
Rachel Jamer <rach.jamer@gmail.com>
2022-04-01 02:16:46 -0400

    GRABBED DATA FROM BACKEND WITH AXIOS

--- react-login/src/pages/Receptionist.js
+++ react-login/src/pages/Receptionist.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 function Receptionist() {
+
+    console.log("hello");
     return (
+
         <div className="Receptionist">
             HELLO THIS IS THE RECEPTIONIST
         </div>
