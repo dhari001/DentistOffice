c6b5e190991a436169095c85cc3b310982aa46d1
Rachel Jamer <rach.jamer@gmail.com>
2022-03-31 23:10:25 -0400

    Added routing + moved Login logic from App.js to Home.js

---
+++ react-login/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className="Navbar">
+            HELLO THIS IS THE NAVBAR
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ react-login/src/pages/Receptionist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Receptionist() {
+    return (
+        <div className="Receptionist">
+            HELLO THIS IS THE RECEPTIONIST
+        </div>
+    );
+}
+
+export default Receptionist;
