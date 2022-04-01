89fe7be3d39641009b13d7ad9af6691916602803
Rachel Jamer <rach.jamer@gmail.com>
2022-04-01 16:55:24 -0400

    Added picture (just for fun)

--- react-login/src/components/BranchDirectory.js
+++ react-login/src/components/BranchDirectory.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {Card, Header} from 'semantic-ui-react'
+import {Card, Header, Image} from 'semantic-ui-react'
 import axios from 'axios'
 import {Link} from 'react-router-dom';
 
@@ -26,14 +26,17 @@ const BranchDirectory = () => {
             <Card.Group>
                 {branches.map((branch) => (
                     <Card href={`/branch/${branch.id}`}>
+                        <Image src='https://images.squarespace-cdn.com/content/v1/5c59c031e666694359ad9dc7/1549558600182-A3A85U6YWLLJP0PX0BXF/Exterior.jpg?format=2500w'/>
                         <Card.Content>
                             <Card.Header>Branch {branch.address_id}</Card.Header>
                             <Card.Description> This is a branch {branch.id} it is managed by {branch.manager_id}</Card.Description>
 
                         </Card.Content>
                     </Card>
+
                 ))}
             </Card.Group>
+
         </>
     )
 
