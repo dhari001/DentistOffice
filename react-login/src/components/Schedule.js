8c0cfcf479bbef16ae3fc1951cc9acf731a37cf9
Dharitri Dixit <dharidix@gmail.com>
2022-04-06 12:48:41 -0400

    search-broken

--- react-login/src/components/Schedule.js
+++ react-login/src/components/Schedule.js
@@ -1,17 +1,47 @@
 import React, { useEffect, useState } from "react";
-import {Card, Table} from 'semantic-ui-react'
+import {Table, Form, Button} from 'semantic-ui-react'
 import axios from "axios";
+import SearchForm from './SearchForm'
 
 const Schedule = () => {
 
+    var set;
     const [appt, setappt] = useState([]);
+    //const [search, setSearch] = useState("")
+    const Search = details => {
+        //set = details.empId
+        console.log(details.empId)
 
-    const getSchedule = () => {
-        axios.get('http://localhost:8080/appointment/findAll').then(res => {
+    }
+   
+   //var set;
+   /*const submitHandler = e => {
+        e.preventDefault();
+        Search(search);
+        //setDetails(details.empId)
+        //console.log(details.empId)
+        //set = details.empId
+        //return set
+        
+    }*/
+    //console.log(details.empId)
+    //console.log(set)
+
+
+    //var set = details.empId
+    //console.log(set)
+    //const [details, setDetails] = useState({empId:""})
+    const getSchedule = ()=> {
+        //var id = submitHandler
+        
+        axios.get('http://localhost:8080/appointment/findByEmployeeId?id='+"P_2").then(res => {
             setappt(res.data)
             console.log(res.data)
+            //console.log(set)
+            //console.log(search)
         }).catch(function (e){
             console.log(e)
+            //console.log(search)
         });
     }
 
@@ -21,9 +51,10 @@ const Schedule = () => {
 
 
     return (
+        <>
+        <SearchForm Search={Search}/>
 
-        
-            <><h1>Appointment Schedule</h1>
+        <><><h1>Appointment Schedule</h1>
             <Table>
                 <Table.Header>
                     <Table.Row>
@@ -35,19 +66,19 @@ const Schedule = () => {
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                 {appt.map((ap) => (
-                     <Table.Row>
-                         <Table.Cell>{ap.startTime}</Table.Cell>
-                         <Table.Cell>{ap.room}</Table.Cell>
-                         <Table.Cell>{ap.branchId}</Table.Cell>
-                         <Table.Cell>{ap.employeeId}</Table.Cell>
-                         <Table.Cell>{ap.patientId}</Table.Cell>
-                     </Table.Row>
+                    {appt.map((ap) => (
+                        <Table.Row>
+                            <Table.Cell>{ap.startTime}</Table.Cell>
+                            <Table.Cell>{ap.room}</Table.Cell>
+                            <Table.Cell>{ap.branchId}</Table.Cell>
+                            <Table.Cell>{ap.employeeId}</Table.Cell>
+                            <Table.Cell>{ap.patientId}</Table.Cell>
+                        </Table.Row>
 
-                 ))}
-             </Table.Body>
+                    ))}
+                </Table.Body>
             </Table>
-        </>
+        </></></>
             
     );
 
